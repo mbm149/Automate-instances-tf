c2db8eafed8ef00b25a7be1ff5bff692bc0dd8b6
mbm149 <105382530+mbm149@users.noreply.github.com>
2023-04-16 16:01:03 -0700

    fixed issue with script

--- script-tools.sh
+++ script-tools.sh
@@ -5,11 +5,15 @@ sudo apt-get update
 
 #Instal git
 apt-get -y install git
+git clone https://github.com/mbm149/Automate-network-tf.git
 
-#git clone https://github.com/mbm149/Automate-network-tf.git
+cd Automate-network-tf
 
-#cd Automate-network-tf
-#./script-tools.sh
+# allow to run .sh file 
+chmod u+x script-tools.sh
+
+#run the script 
+./script-tools.sh
 
 #Install nmap, autopsy, sleuthkit
 sudo apt-get install nmap autopsy sleuthkit -y 
