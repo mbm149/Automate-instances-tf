517fa9790117c9c508a5913bd5406219e7eb69c8
105382530+mbm149@users.noreply.github.com <105382530+mbm149@users.noreply.github.com>
2023-04-16 16:04:28 -0700

    fixed

--- script-tools.sh
+++ script-tools.sh
@@ -4,11 +4,12 @@
 sudo apt-get update
 
 #Instal git
-apt-get -y install git
-
+#apt-get -y install git
 #git clone https://github.com/mbm149/Automate-network-tf.git
-
 #cd Automate-network-tf
+# allow to run .sh file 
+#chmod u+x script-tools.sh
+#run the script 
 #./script-tools.sh
 
 #Install nmap, autopsy, sleuthkit
@@ -19,4 +20,3 @@ sudo snap install joplin-james-carroll
 
 #Intasll volatility-phocean
 sudo snap install volatility-phocean -y 
-
